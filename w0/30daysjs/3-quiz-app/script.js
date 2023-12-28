b551373b3a1886ea50fc80c9b11352c78adb386a
Gauri <gaurisinghh3@gmail.com>
2023-12-28 19:12:58 +0530

    quiz complete

--- w0/30daysjs/3-quiz-app/script.js
+++ w0/30daysjs/3-quiz-app/script.js
@@ -38,13 +38,16 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
+
+    resetState();
+
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
@@ -52,7 +55,6 @@ function startQuiz() {
 }
 
 function showQuestion() {
-
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -61,8 +63,67 @@ function showQuestion() {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
+// remove all the previous answers
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+    answerButtons.innerHTML = "";
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct == "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `Your Score is: ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "Play Again?";
+    nextButton.style.display = "block";
+    nextButton.addEventListener("click", startQuiz);
+}
+
+function handleNextButton() {
+    resetState();
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+
+
+nextButton.addEventListener("click", () => {
+    handleNextButton();
+});
+
+
+
 
 startQuiz();
