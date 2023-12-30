d76e8b17edcba15c572a82e6fc6a05a762540389
Gauri <gaurisinghh3@gmail.com>
2023-12-30 17:48:13 +0530

    w7 complete

--- w0/30daysjs/7-quote-generator/script.js
+++ w0/30daysjs/7-quote-generator/script.js
@@ -3,10 +3,21 @@ const author = document.getElementById("author");
 const apiUrl = "https://api.quotable.io/quotes/random";
 
 async function getQuote(url) {
-    const response = await fetch(url);
-    var data = await response.json();
-    quote.innerHTML = data.content;
-    author.innerHTML = data.author;
+    try {
+        const response = await fetch(url);
+        var data = await response.json();
+        console.log(data)
+        quote.innerHTML = data[0].content;
+        author.innerHTML = data[0].author;
+    }
+    catch {
+        console.log("Error fetching quote from API");
+    }
+
 }
 
 getQuote(apiUrl)
+
+function tweet() {
+    window.open(`https://twitter.com/intent/tweet?text=${quote.innerHTML}` + ` --by ${author.innerHTML}`, "Tweet Windoe", "width=600", "height=300")
+}
